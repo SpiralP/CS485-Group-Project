e7080ad3eb3e5db449dd0eb1103d3f51cb6a4b57
mark-rahal <mgr0904@gmail.com>
2020-04-17 16:49:10 -0700

    Created game scene

---
+++ Assets/Scripts/AsteroidCulling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CollisionDeleteEvent : UnityEvent<GameObject>
+{
+}
+
+public class AsteroidCulling : MonoBehaviour
+{
+  public CollisionDeleteEvent asteroidCollide;
+  // Start is called before the first frame update
+  void Start()
+  {
+    asteroidCollide = new CollisionDeleteEvent();
+    asteroidCollide.AddListener(Collide);
+  }
+
+
+  // Update is called once per frame
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Asteroid")
+    {
+      asteroidCollide.Invoke(collision.gameObject);
+    }
+  }
+
+  void Collide(GameObject asteroid)
+  {
+    Destroy(asteroid);
+  }
+}
---
+++ Assets/Scripts/ShipCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CollisionEvent : UnityEvent<GameObject>
+{
+}
+
+public class ShipCollision : MonoBehaviour
+{
+  public CollisionEvent asteroidCollide;
+  // Start is called before the first frame update
+  void Start()
+  {
+    asteroidCollide = new CollisionEvent();
+    asteroidCollide.AddListener(Collide);
+  }
+
+
+  // Update is called once per frame
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Asteroid")
+    {
+      asteroidCollide.Invoke(collision.gameObject);
+    }
+  }
+
+  void Collide(GameObject asteroid)
+  {
+    Destroy(asteroid);
+  }
+}
